6b8e57eed8caf81d12dcf7af04553bfc7b3ae3c6
f416720001 <f416720001@yahoo.com.tw>
2018-12-26 07:25:46 +0800

    ver4.1 add "go back" button

--- js/ScrolltoTop.js
+++ js/ScrolltoTop.js
@@ -1,26 +1,21 @@
-//Make sure the user has scrolled at least double the height of the browser
-var toggleHeight = $(window).outerHeight() * 2;
+$(document).ready(function(){
+	// hide #back-top first
+	$("#scroll-top").hide();
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#scroll-top').fadeIn();
+			} else {
+				$('#scroll-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#scroll-top button').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+		});
+	});
 
-$(window).scroll(function() {
-	if ($(window).scrollTop() > toggleHeight) {
-		//Adds active class to make button visible
-		$(".m-backtotop").addClass("active");
-		
-		//Just some cool text changes
-		$('h1 span').text('TA-DA! Now hover it and hit dat!')
-		
-	} else {
-		//Removes active class to make button visible
-		$(".m-backtotop").removeClass("active");
-		
-		//Just some cool text changes
-		$('h1 span').text('(start scrolling)')
-	}
-});
-
-
-//Scrolls the user to the top of the page again
-$(".m-backtotop").click(function() {
-	$("html, body").animate({ scrollTop: 0 }, "slow");
-	return false;
 });
